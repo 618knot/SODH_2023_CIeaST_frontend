def67a0965184bbcabac2187b05b4a1b6156df85
618knot <publicac.clt618knot@gmail.com>
2023-11-14 18:39:07 +0900

    emailのplaceholder追加

--- src/view/signup/SignUp.tsx
+++ src/view/signup/SignUp.tsx
@@ -18,6 +18,7 @@ const SignUp = () => {
             <Input
                 id="email"
                 label="メールアドレス"
+                placeHolder="cieast@example.com"
             />
             <Input
                 id="password"
