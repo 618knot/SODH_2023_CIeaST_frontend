1595063d3275bf030e256290d4c6998cba2f930c
618knot <publicac.clt618knot@gmail.com>
2023-11-18 03:18:30 +0900

    ユーザ登録ロジックの実装

--- src/view/signup/SignUp.tsx
+++ src/view/signup/SignUp.tsx
@@ -2,6 +2,8 @@ import { CSSProperties } from "react";
 import Button from "../../components/button/Button";
 import Input from "../../components/input/Input";
 import "./SignUp.scss";
+import { errorNotification, successNotification } from "../../util/notification";
+import axios from "../../util/axios";
 
 const style: CSSProperties = {
     width: "300px",
@@ -28,9 +30,41 @@ const SignUp = () => {
                 label="登録"
                 className="primary"
                 style={style}
+                onClick={ onClick }
             />
         </div>
     );
 };
+
+// TODO: フロント側のバリデーション実装
+const onClick = () => {
+    const inputElements = document.getElementsByTagName("input");
+    const elementsArray = Array.from(inputElements);
+    const elementValues: any[] = [];
+    const requestKeys = ["name", "email", "password"];
+
+    elementsArray.forEach(element => {
+        elementValues.push(element.value);
+    });
+
+    const requestData = Object.fromEntries(requestKeys.map((key, index) => [key, elementValues[index]]));
+    console.log(requestData)
+
+    axios.post("/users/register", requestData).then(
+        (response) => {
+            console.log(response.data["status"])
+            if(response.data["status"] == "error") {
+                errorNotification(response.data["message"]);
+            } else {
+                successNotification("登録に成功しました");
+                window.location.href = "/login";
+            }
+        },
+        (response) => {
+            console.log(response)
+            errorNotification("登録に失敗しました");
+        }
+    )
+}
 
 export default SignUp;
