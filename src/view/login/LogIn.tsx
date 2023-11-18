19f510dedc2f8bb7dde57b3f01a064acb9626d3c
618knot <publicac.clt618knot@gmail.com>
2023-11-18 12:50:01 +0900

    ログインロジックの実装

--- src/view/login/LogIn.tsx
+++ src/view/login/LogIn.tsx
@@ -2,6 +2,9 @@ import { CSSProperties } from "react";
 import Button from "../../components/button/Button";
 import Input from "../../components/input/Input";
 import "./LogIn.scss";
+import axios from "../../util/axios";
+import { errorNotification, successNotification } from "../../util/notification";
+import { Cookies, useCookies } from "react-cookie";
 
 const style: CSSProperties = {
     width: "300px",
@@ -22,17 +25,53 @@ const LogIn = () => {
             />
             <div className="button-container">
                 <Button
-                    label="登録"
+                    label="ログイン"
                     className="primary"
                     style={style}
+                    onClick={ onClick }
                 />
                 <Button
                     label="新規登録"
                     className="secondary"
                     style={style}
+                    onClick={ () => { window.location.href = "/signup" } }
                 />
             </div>
         </div>
+    );
+};
+
+const onClick = () => {
+    const inputElements = document.getElementsByTagName("input");
+    const elementsArray = Array.from(inputElements);
+    const elementValues: any[] = [];
+    const requestKeys = ["email", "password"];
+
+    elementsArray.forEach(element => {
+        elementValues.push(element.value);
+    });
+
+    const requestData = Object.fromEntries(requestKeys.map((key, index) => [key, elementValues[index]]));
+
+    axios.post("/sessions/login", requestData).then(
+        (response) => {
+            console.log(response.data)
+            if(response.data["status"] == "error") {
+                errorNotification(response.data["message"]);
+            } else {
+                successNotification("ログインに成功しました");
+
+                // cookieの書き込み
+                const cookies = new Cookies();
+                cookies.set("session_id", response.data["session id"]);
+
+                window.location.href = "/";
+            }
+        },
+        (response) => {
+            console.log(response)
+            errorNotification("ログインに失敗しました");
+        }
     );
 };
 
