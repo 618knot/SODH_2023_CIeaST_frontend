447cbc61f04c41b97b0aee7aa98dec6bb46816df
618knot <publicac.clt618knot@gmail.com>
2023-11-18 17:06:37 +0900

    fix isLogin

--- src/util/isLogin.ts
+++ src/util/isLogin.ts
@@ -4,8 +4,7 @@ const cookies = new Cookies();
 
 const isLogin = () => {
     const cookieValue = cookies.get("session_id");
-    console.log(window.location.pathname)
-    if (!cookieValue) {
+    if (cookieValue === undefined && !(window.location.pathname == "/login" || window.location.pathname == "/signup")) {
         window.location.href = "/login";
     } else if (cookieValue && (window.location.pathname == "/login" || window.location.pathname == "/signup")) {
         window.location.href = "/";
